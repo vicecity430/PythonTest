8efe34310d6d1308e0dc9b72ef3f24422df95be5
jimmy74185 <jimmy03180318@gmail.com>
2023-07-06 14:33:26 +0800

    This is test commit for MMZ TODO: learning

---
+++ 796.rotate-string.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=796 lang=python3
+#
+# [796] Rotate String
+#
+
+# @lc code=start
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        for i in range(len(goal)):
+            if s == (goal[0:i]+goal[i:]):
+                return True
+            rgoal = goal[::-1]
+            if s == (rgoal[0:i]+rgoal[i:]):
+                return True
+        return False
+# @lc code=end
+
